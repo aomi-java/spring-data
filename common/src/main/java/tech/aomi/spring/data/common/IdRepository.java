7841a251870a06aecae846963946d060239f3b49
尼古拉斯丶少 <sean.snow@live.com>
2022-05-01 10:58:55 +0800

    docs: 接口说明

--- common/src/main/java/tech/aomi/spring/data/common/IdRepository.java
+++ common/src/main/java/tech/aomi/spring/data/common/IdRepository.java
@@ -2,12 +2,25 @@ package tech.aomi.spring.data.common;
 
 
 /**
- * @author Sean(sean.snow @ live.com) createAt 17-12-26.
+ * id 仓库
+ *
+ * @author Sean createAt 17-12-26.
  */
 public interface IdRepository {
 
+    /**
+     * 创建seq
+     *
+     * @param sequenceName 序列名称
+     */
     void createSequence(String sequenceName);
 
+    /**
+     * 根绝序列名称获取id值
+     *
+     * @param sequenceName 序列名称
+     * @return value
+     */
     Long generateSequence(String sequenceName);
 
 }
